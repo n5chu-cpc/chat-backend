64209c064d8dc8b2da63e662e007ddc19f2a8602
なおちゃん <166379976+naochan83275@users.noreply.github.com>
2024-08-27 18:18:32 +0900

    Create server.js

---
+++ server.js
@@ -0,0 +1,13 @@
+// server.js
+const WebSocket = require('ws');
+const server = new WebSocket.Server({ port: 8080 });
+
+server.on('connection', socket => {
+    socket.on('message', message => {
+        server.clients.forEach(client => {
+            if (client.readyState === WebSocket.OPEN) {
+                client.send(message);
+            }
+        });
+    });
+});
