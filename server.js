51b1fdbd8b07bcf07081ca223840a159c7d50089
なおちゃん <166379976+naochan83275@users.noreply.github.com>
2024-08-28 19:57:06 +0900

    Update server.js

--- server.js
+++ server.js
@@ -1,12 +1,22 @@
 const WebSocket = require('ws');
 const server = new WebSocket.Server({ port: 8080 });
 
-server.on('connection', socket => {
-    socket.on('message', message => {
+server.on('connection', (socket) => {
+    console.log('Client connected');
+
+    socket.on('message', (message) => {
+        console.log('Received:', message);
+        // Broadcast message to all clients
         server.clients.forEach(client => {
             if (client.readyState === WebSocket.OPEN) {
                 client.send(message);
             }
         });
     });
+
+    socket.on('close', () => {
+        console.log('Client disconnected');
+    });
 });
+
+console.log('WebSocket server is running on ws://localhost:8080');
